4f6a16a76d3f2b173a953a4dee649cc0ed39b003
achjon <achjon338@gmail.com>
2022-07-15 11:57:04 +0200

    Added a eraser option

--- script/main.js
+++ script/main.js
@@ -2,20 +2,21 @@ var x;
 var y;
 var ctx;
 var c;
+var gumaobject;
 
 
 function Draw() {
-c = document.getElementById("whiteboard");
-ctx = c.getContext("2d");
-c.setAttribute('width', '800');
-c.setAttribute('height', '600');
+    c = document.getElementById("whiteboard");
+    ctx = c.getContext("2d");
+    c.setAttribute('width', '800');
+    c.setAttribute('height', '600');
 
-c.width = document.body.clientWidth;
-c.height = document.body.clientHeight; 
+    c.width = document.body.clientWidth;
+    c.height = document.body.clientHeight;
 
-ctx.lineWidth = 5;
-ctx.strokeStyle = '#000000';
-document.getElementById("brush").innerHTML = "Brush size: "+ctx.lineWidth;
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#000000';
+    document.getElementById("brush").innerHTML = "Brush size: " + ctx.lineWidth;
 
 }
 
@@ -26,57 +27,59 @@ function showCoords(event) {
     y = event.clientY + window.scrollY;
     var coor = "X: " + x + ", Y: " + y;
 
-    
-  }
 
-function black(){
-ctx.strokeStyle = '#000000';
 }
 
-function blue(){
+function black() {
+    ctx.strokeStyle = '#000000';
+    gumaobject = false;
+}
+
+function blue() {
     ctx.strokeStyle = ' #3498db';
+    gumaobject = false;
 }
 
-function red(){
+function red() {
     ctx.strokeStyle = '#c0392b';
+    gumaobject = false;
 }
 
-function clean(){
+function clean() {
     console.log("cleaning...")
     ctx.clearRect(0, 0, c.width, c.height);
 }
 
-function plus(){
-    ctx.lineWidth = ctx.lineWidth + 1
-    document.getElementById("brush").innerHTML = "Brush size: "+ctx.lineWidth;
+function plus() {
+    ctx.lineWidth = ctx.lineWidth + 1;
+    document.getElementById("brush").innerHTML = "Brush size: " + ctx.lineWidth;
 }
 
-function minus(){
-    ctx.lineWidth = ctx.lineWidth - 1
-    if (ctx.lineWidth = ctx.lineWidth - 1 < 1){
+function minus() {
+    ctx.lineWidth = ctx.lineWidth - 1;
+    if (ctx.lineWidth = ctx.lineWidth - 1 < 1) {
+        ctx.lineWidth = 1;
+    }
+    document.getElementById("brush").innerHTML = "Brush size: " + ctx.lineWidth;
+
+}
+
+function plusplus() {
+    ctx.lineWidth = ctx.lineWidth + 5;
+    document.getElementById("brush").innerHTML = "Brush size: " + ctx.lineWidth;
+}
+
+function minusminus() {
+    ctx.lineWidth = ctx.lineWidth - 5;
+    if (ctx.lineWidth = ctx.lineWidth - 1 < 1) {
         ctx.lineWidth = 1
     }
-    document.getElementById("brush").innerHTML = "Brush size: "+ctx.lineWidth;
+    document.getElementById("brush").innerHTML = "Brush size: " + ctx.lineWidth;
 
 }
 
-function plusplus(){
-    ctx.lineWidth = ctx.lineWidth + 5
-    document.getElementById("brush").innerHTML = "Brush size: "+ctx.lineWidth;
-}
-
-function minusminus(){
-    ctx.lineWidth = ctx.lineWidth - 5
-    if (ctx.lineWidth = ctx.lineWidth - 1 < 1){
-        ctx.lineWidth = 1
-    }
-    document.getElementById("brush").innerHTML = "Brush size: "+ctx.lineWidth;
-
-}
-
-function exportovat(){
-    var img = c.toDataURL('image/png')
-
+function exportovat() {
+    var img = c.toDataURL('image/png');
     var download = document.createElement('a');
     download.href = img;
     download.download = "whiteboard.png";
@@ -84,15 +87,42 @@ function exportovat(){
 
 }
 
-function penDown(event){
-    
-  if (event.buttons !== 1) return;
-  ctx.beginPath();
-  ctx.lineCap = 'round';
-  ctx.moveTo(x, y);
-  showCoords(event);
-  ctx.lineTo(x, y);
-  ctx.stroke();
+function guma() {
+    gumaobject = true;
 }
 
 
+c.addEventListener("touchstart", function(e) {
+    mousePos = getTouchPos(c, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    });
+    c.dispatchEvent(mouseEvent);
+}, false);
+
+document.body.addEventListener("touchstart", function(e) {
+    if (e.target == c) {
+        e.preventDefault();
+    }
+}, false);
+
+function penDown(event) {
+
+    if (event.buttons !== 1) return;
+
+    if (gumaobject == true) {
+        console.log(ctx.lineWidth);
+        showCoords(event);
+        ctx.clearRect(x - (ctx.lineWidth / 2), y - (ctx.lineWidth / 2), ctx.lineWidth, ctx.lineWidth);
+        console.log("guma");
+    } else {
+        ctx.beginPath();
+        ctx.lineCap = 'round';
+        ctx.moveTo(x, y);
+        showCoords(event);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
